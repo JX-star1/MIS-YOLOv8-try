946f23b2e93c0e8035521c6b99b9afa026cd67d8
JX-star1 <1782578121@qq.com>
2026-03-12 11:06:28 +0800

    Add MFE to parse_model argument handling

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -875,6 +875,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             C1,
             C2,
             C2f,
+            MFE,
             RepNCSPELAN4,
             ADown,
             SPPELAN,
